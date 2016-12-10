c106fcf0b985f501654a3be6def65ed5b6fda6b6
Henri83 <henri.gouraud@laposte.net>
2016-12-10 14:36:26 +0100

    Reorganization of images/src/etc under the same mybase.gwb directory
    * trace of opened files in secure.ml left in comment if needed again

--- src/srcfile.ml
+++ src/srcfile.ml
@@ -177,12 +177,13 @@ value source_file_name conf fname =
   let bname = conf.bname in
   let lang = conf.lang in
   let fname1 =
-    List.fold_right Filename.concat [Util.base_path ["src"] bname; lang]
+    (*List.fold_right Filename.concat [Util.base_path ["src"] bname; lang]*)
+    List.fold_right Filename.concat [Util.base_path [] (bname ^ ".gwb/src"); lang]
       (Filename.basename fname ^ ".txt")
   in
   if Sys.file_exists fname1 then fname1
   else
-    Filename.concat (Util.base_path ["src"] bname)
+    Filename.concat (Util.base_path [] (bname ^ ".gwb/src"))
       (Filename.basename fname ^ ".txt")
 ;
 
